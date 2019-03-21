ffc311ececec3e263e26d709f5e1e272169756e0
Adele <gauvrit.adele@gmail.com>
2019-03-21 14:52:32 +0100

    moved list of pokemon to json file

--- src/Controller/PokemonController.php
+++ src/Controller/PokemonController.php
@@ -14,19 +14,7 @@ class PokemonController
    */
   public function get_list()
   {
-    return new JsonResponse([
-        '1' => [
-            'name' => 'bulbasaur',
-            'url' => 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png'
-        ],
-        '2' => [
-                'name' => 'ivysaur',
-                'url' => 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/2.png'
-            ],
-        '3' => [
-                'name' => 'venusaur',
-                'url' => 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/3.png'
-        ]
-    ]);
+    $json = file_get_contents("../src/pokemon.json");
+    return new JsonResponse($json);
   }
 }
